20cb979d4fcc32610ef9f937771bd3354f1c7931
leensungzero <brianwoo119@naver.com>
2019-01-09 21:14:28 +0900

    [#163006864 Finished] Signup API - postman test 완료

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('api/', include('djoser.urls')),
     path('api/', include('djoser.urls.authtoken')),
     path('api/', include('djoser.urls.jwt')),
