fd0188bbd002726d3f9ada6c89e52ffc26ac861a
kill5038 <blain1@naver.com>
2019-01-14 14:50:51 +0900

    [#163008503 finished] 프로젝트 rds 연결 나중에 이건 dev모드 prod모드 나눠야 할듯

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,8 +86,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'schedule_prod',
+        'USER': 'root',
+        'PASSWORD': 'whgusdnr12',
+        'HOST': 'playgillround.cwadawgcd2ch.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
