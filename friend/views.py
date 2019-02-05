84898532f868965de319e9991a43a5c4c6df7599
leensungzero <brianwoo119@naver.com>
2019-02-05 13:03:14 +0900

    [#163175864] user model과 friend model 연결 실패 => api로 따로 구성

--- friend/views.py
+++ friend/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import get_user_model
 
+from django.db.models import Q
+
 from .models import FriendRequest, Friend
 
 from .serializers import FriendRequestSerializer, FriendSerializer
 
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
 User = get_user_model()
@@ -54,3 +57,25 @@ class FriendRequestViewSet(viewsets.ModelViewSet):
 class FriendViewSet(viewsets.ModelViewSet):
     queryset = Friend.objects.all()
     serializer_class = FriendSerializer
+
+    @action(detail=False)
+    def filter(self, request):
+        try:
+            user = request.GET.get('user')
+
+            try:
+                user = User.objects.get(nick_name=user)
+            except:
+                return Response({
+                    'message': '잘못된 요청'
+                })
+
+            friends = Friend.objects.filter(Q(request_user=user) | Q(response_user=user))
+
+            serializer = self.get_serializer(friends, many=True)
+
+            return Response(serializer.data)
+        except:
+            return Response({
+                'message': '잘못된 요청'
+            })
